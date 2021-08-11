d25b9d61c273d97ad8abde996fecdcee6e2784b8
Ippei-iwashiro <83683755+Ippei-iwashiro@users.noreply.github.com>
2021-08-11 10:43:28 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>hello world</h1>
+      <p>name need roop append error comisito staition...</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
